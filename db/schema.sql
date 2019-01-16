7db862ca90117b5b670adad9f23e5315be7b2620
Conte <coding@Contes-MacBook-Pro.local>
2019-01-16 12:29:38 -0800

    updated database in schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,9 @@ CREATE TABLE activities (
     event_city VARCHAR(50),
     event_link VARCHAR (150), 
     createdAt timeStamp default CURRENT_TIMESTAMP NOT NULL, 
-    event_category VARCHAR(35)
+    event_category VARCHAR(35),
+    user_id INT(5) NOT NULL,
+    post_id INT (10) NOT NULL
 );
 
 CREATE TABLE users (
@@ -17,5 +19,12 @@ CREATE TABLE users (
   interests VARCHAR( 255 ) NOT NULL,
   city VARCHAR (255) NOT NULL
 );
+
+CREATE TABLE interests (
+  id Int(7) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_id INT(5) NOT NULL,
+  user_interest varchar(300)
+);
+
 
 SELECT * FROM  activities;
