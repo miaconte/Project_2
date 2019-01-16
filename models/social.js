72d1b6889e9054be65c92a865a88e2d5432936dc
Kalasky <kalaskyr@gmail.com>
2019-01-16 12:10:21 -0800

    Models | Created Handlebars files

--- models/social.js
+++ models/social.js
@@ -1,2 +1,11 @@
 var sequelize = require("sequelize");
 
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define("activities", {
+        event_name: DataTypes.STRING,
+        event_city: DataTypes.STRING,
+        event_link: DataTypes.STRING,
+        event_category: DataTypes.STRING,
+        post_id: DataTypes.INTEGER
+    });
+};
