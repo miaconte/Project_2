bcc88a58f7bf56a7e3378905048478f5f6c0e7c0
Conte <coding@Contes-MacBook-Pro.local>
2019-01-17 11:16:32 -0800

    updated handlebar temps

--- controllers/socialController.js
+++ controllers/socialController.js
@@ -1,31 +1,30 @@
-// var express = require("express");
-// var app = express();
-// var router = express.Router();
+var express = require("express");
+var app = express();
+var router = express.Router();
 
-// // Import the model (social.js) to use its database functions.
-// var social = require("../models/social.js");
+// Import the model (social.js) to use its database functions.
+var social = require("../models/social.js");
 
 
-// function index(req, res) {
-//     app.get("/", function (res, req) {
-//         res.sendfile(path.join(__dirname, ("./landing.html")))
-//     })
-// };
+router.get("/", function(res){
+    res.sendFile(path.join(__dirname, "../landing.html"));
+});
 
-// router.get("/activities", function (req, res) {
-//     social.all(function (data) {
-//         var actObj = {
-//             activities: data
-//         };
-//         console.log(actObj);
-//         res.render("activities", actObj);
-//     });
-// });
 
-// router.put("/api/activities/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
+router.get("/activities", function (req, res) {
+    social.all(function (data) {
+        var actObj = {
+            activities: data
+        };
+        console.log(actObj);
+        res.render("activities", actObj);
+    });
+});
+
+router.put("/api/activities/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
   
-//     console.log("condition", condition);
-// });
+    console.log("condition", condition);
+});
 
-// module.exports = router;
+module.exports = router;
