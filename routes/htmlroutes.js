04c2b7b86e6ffcbd2720d4f54b0b201518c99ed1
Kalasky <kalaskyr@gmail.com>
2019-01-18 14:05:10 -0800

    Resolved Handlebars issue as well as city parameter

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -8,8 +8,9 @@ module.exports = function(app) {
 
     // will display survey page when /survey is visited.
     app.get("/activities/:city", function(req, res){
-        var city = req.params.city 
-        console.log(city);
+        var city = req.params.city;
+        const hbsObject  = {city};
+        res.render("activities", hbsObject);
     });
 
     // default catches undefined routes and send user to home page. 
